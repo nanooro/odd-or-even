39478bdbbf724001ae8ee713279295e83be9463b
nanooru <notnotsky26@gmail.com>
2024-10-31 18:18:12 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,18 @@
+function update(){
+    let inputNumber = document.getElementById("numberinput");
+    let whoknows = inputNumber.value;
+    let output = document.getElementById("inputtednum1");
+    let output2 = output.textContent;
+    output.textContent = whoknows;
+    if (isNaN(whoknows)){
+        output.textContent = "please enter a number";
+    }else{
+        output.textContent = "ur number is odd"; 
+        if (whoknows % 2 === 0){
+            output.textContent = "ur number is even";         
+        }else{
+            
+        }
+    }   
+
+}
