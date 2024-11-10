f9acd7afe2de182548e92ccbfef60ee811a1852b
nanooru <notnotsky26@gmail.com>
2024-11-10 16:25:29 +0530

    Add files via upload
    finally fixed my website

--- index.js
+++ index.js
@@ -1,18 +1,25 @@
-// function update(){
-//     let inputNumber = document.getElementById("numberinput");
-//     let whoknows = inputNumber.value;
-//     let output = document.getElementById("inputtednum1");
-//     let output2 = output.textContent;
-//     output.textContent = whoknows;
-//     if (isNaN(whoknows)){
-//         output.textContent = "please enter a number";
-//     }else{
-//         output.textContent = "ur number is odd"; 
-//         if (whoknows % 2 === 0){
-//             output.textContent = "ur number is even";         
-//         }else{
-            
-//         }
-//     }   
+function submit() {
+  let referenceOfUserInput = document.getElementById("userInput");
+  let userInput = referenceOfUserInput.value;
+  // console.log("user has entered " + userInput);
+  // this above code gets what the user has typed
+  let referenceOfResult = document.getElementById("output");
+  let result = referenceOfResult.innerText;
+  // console.log(result);
+  // this code get the text of the result para
+  if (isNaN(userInput)) {
+    console.log("user has not entered a number ");
+    referenceOfResult.innerText = "Please enter a number";
+  } else {
+    //checks if num is even
+    console.log("user entered number ");
+    if ((userInput & 1) === 0) {
+      console.log("yup user entered a number that is divisible by 2 ");
+      referenceOfResult.innerText = "ur number is even";
+    } else {
+      console.log("nope user entered a number that is not divisible by 2 ");
+      referenceOfResult.innerText = "ur number is odd";
+    }
+  }
 
-// }
+}
